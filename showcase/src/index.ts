a553846c25d51da6f1bdd60ec9ce08fc6bbe20b4
Guillaume Fontorbe <guillaume.fontorbe@typefox.io>
2023-05-23 14:22:27 +0200

    Fit action and build script
    Signed-off-by: Guillaume Fontorbe <guillaume.fontorbe@typefox.io>

--- showcase/src/index.ts
+++ showcase/src/index.ts
@@ -15,6 +15,7 @@
  ********************************************************************************/
 
 import "reflect-metadata";
+import {CenterAction, FitToScreenAction} from "sprotty-protocol";
 import { LocalModelSource, TYPES } from "sprotty";
 import createASCETContainer from "./ascet-example/di.config";
 import createLangiumSprottyContainer from "./langium-diagram/diagram-frontend/di.config";
@@ -26,11 +27,13 @@ export default function runExample() {
     const ascetContainer = createASCETContainer('sprotty-ascet-example');
     const ascetModelSource = ascetContainer.get<LocalModelSource>(TYPES.ModelSource);
     ascetModelSource.setModel(model);
+    ascetModelSource.actionDispatcher.dispatch(<FitToScreenAction>{kind: 'fit', elementIds: [], animate: false, padding: 60})
 
     const client = createMonacoEditor('monaco-editor');
     const langiumContainer = createLangiumSprottyContainer('sprotty-langium-diagram', client.getLanguageClient());
     const langiumModelSource = langiumContainer.get<LSWorkerDiagramServerProxy>(TYPES.ModelSource);
     langiumModelSource.start();
+    langiumModelSource.actionDispatcher.dispatch(<FitToScreenAction>{kind: 'fit', elementIds: [], animate: false, padding: 50})
 }
 
 document.addEventListener("DOMContentLoaded", () => runExample());
